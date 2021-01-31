58ad5dc16154bce250082d6e2944007e99335f5b
Jithin Sathyan <jithinsathyan0709@gmail.com>
2021-01-31 20:23:27 +0530

    1. Attribute binding. 2. using v-bind the title as description from data.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="course-container">
+      <div class="header">
+        <span class="brand">Study Buddy</span>
+        <div class="total-enrollments" title="Your enrollments">
+          {{ currentEnrollments }}
+        </div>
+      </div>
+      <div class="course">
+        <div class="course-main">
+          <div class="course-title" v-bind:title="description">{{ name }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      name: 'Vue.js',
+      enrollments: 50,
+      currentEnrollments: 0,
+      description: 'A basic course to understand the concept of Vue JS',
+    };
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
