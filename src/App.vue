bac5f867f8bf6e22641d36fea4e93b58a70d8b3d
Jithin Sathyan <jithinsathyan0709@gmail.com>
2021-03-20 22:06:21 +0530

    Minor CSS fix

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ export default {
   font-family: "Blinker", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  background: #ede7f6;
 }
 
 .course-container {
